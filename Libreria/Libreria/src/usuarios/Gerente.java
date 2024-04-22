5cad7bf5010f525890b67ff5468a426b5151df3e
Ara Leilani Muñoz Martinez <aramunoz65m@gmail.com>
2024-04-22 14:46:36 -0600

    Se subio el proyecto refactorizado

---
+++ Libreria/Libreria/src/usuarios/Gerente.java
@@ -0,0 +1,98 @@
+
+package usuarios;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import static libreria.Libreria.usuarios;
+import libreria.utils.DatosComun;
+import usuarios.utils.Rol;
+import utils.UsuarioEnSesion;
+
+public class Gerente extends Usuario{
+   
+    private Double sueldo;
+    private String INE;
+
+    public Gerente(Double sueldo, String INE, String nombre, String apellido, String telefono,String nombreUsuario, String contrasena) {
+        super(nombre, apellido, telefono, Rol.GERENTE,nombreUsuario, contrasena);
+        this.sueldo = sueldo;
+        this.INE = INE;
+    }
+    
+     @Override
+    public String toString (){
+      return String.format("%s, \n Sueldo: %f, \n INE: %s", super.toString(),  sueldo, INE);
+  }
+    
+    
+    
+    
+    public static void registrarGerente(){
+        Scanner leer = new Scanner(System.in);
+        String datos[] = DatosComun.registrarUsuario(Rol.GERENTE);
+        String nombre = datos[0];
+        String apellido = datos[1];
+        String telefono = datos[2];
+        String nombreUsuario = datos[3]; 
+        String contrasena = datos[4];
+        System.out.print("Ingresa la INE: ");
+        String ine = leer.nextLine();
+        System.out.print("Ingresa el sueldo: ");
+        Double sueldo = leer.nextDouble();
+        Gerente gerente = new Gerente ( sueldo, ine,nombre,  apellido,  telefono, nombreUsuario,  contrasena);
+        
+        System.out.println("El gerente ha sido registrado correctamente. ");
+       if(!usuarios.containsKey(Rol.GERENTE)){
+            usuarios.put(Rol.GERENTE, new ArrayList<Usuario>());
+        }
+        usuarios.get(Rol.GERENTE).add(gerente);
+        leer.nextLine();
+    }
+    
+    
+    
+   
+    public static void mostrarGerentes(){
+        Scanner leer = new Scanner(System.in);
+        System.out.println("**Gerentes**");
+      
+            for(Usuario usuario: usuarios.get(Rol.GERENTE)){  
+                Gerente gerente = (Gerente)usuario;
+                System.out.println(gerente.toString());        
+        }
+        
+        
+    }
+  
+   
+    public static  void eliminarGerente(){
+        Scanner leer = new Scanner(System.in);
+        if(UsuarioEnSesion.obtenerInstancia().getUsuarioActual().getRol()==Rol.GERENTE){
+            System.out.print("Ingresa el nombre de usuario a eliminar:  ");
+            String usuarioEliminar = leer.nextLine();
+       
+            ArrayList<Usuario> gerentes = usuarios.get(Rol.GERENTE);
+            if (gerentes != null) {
+            boolean usuarioEncontrado = false;
+            for (int i = 0; i < gerentes.size(); i++) {
+                Usuario usuario = gerentes.get(i);
+                if (usuario.getNombreUsuario().equals(usuarioEliminar)) {
+                    gerentes.remove(i);
+                    usuarioEncontrado = true;
+                    System.out.println("El usuario ha sido eliminado");
+                    break;
+                }
+            }
+            if (!usuarioEncontrado) {
+                System.out.println("No se encontró ningún usuario con el nombre especificado.");
+            }
+        } else {
+            System.out.println("No hay gerentes registrados en la librería.");
+        }
+    } else {
+        System.out.println("No tienes permiso asignado para eliminar gerentes.");
+    }
+            
+       
+    }
+}
---
+++ Libreria/Libreria/src/usuarios/Usuario.java
@@ -0,0 +1,71 @@
+
+package usuarios;
+
+import usuarios.utils.Rol;
+
+public class Usuario {
+    private static int CANTIDAD_USUARIOS = 1;
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private Rol rol; //Cliente, asistente, gerente.
+    private String nombreUsuario;
+    private String contrasena;
+
+    public Usuario(String nombre, String apellido, String telefono, Rol rol, String nombreUsuario, String contrasena) {
+        this.id=CANTIDAD_USUARIOS;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.rol = rol;
+        this.contrasena= contrasena;
+        this.nombreUsuario= nombreUsuario;
+        CANTIDAD_USUARIOS++;
+    }
+    
+//    public String mostrarInfo(){
+//        return String.format("ID: %d, Nombre completo %s %s, telefono: %s, rol: %s",
+//                id, nombre, apellido, telefono, rol);
+//    }
+
+    @Override
+    public String toString(){
+       return String.format("ID: %d, \n Nombre completo %s %s, \n telefono: %s, \n rol: %s",
+               id, nombre, apellido, telefono, rol);
+    }
+    public static int getCANTIDAD_USUARIOS() {
+        return CANTIDAD_USUARIOS;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public Rol getRol() {
+        return rol;
+        
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+    
+    
+}
