e2a13e2614a74fc28f4773b92501c9e64d4359ac
Ara Leilani Muñoz Martinez <aramunoz65m@gmail.com>
2024-05-11 14:26:02 -0600

    Se subio avance de Libreria

---
+++ Libreria/Libreria/src/Libros/LibroComedia.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Libros;
+
+import Libros.utils.LibrosUtils;
+import Libros.utils.constants.ClasificacionLibro;
+import Libros.utils.constants.Genero;
+import Libros.utils.constants.TerrorSubgenero;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author leila_pe8dh8w
+ */
+public class LibroComedia extends Libro{
+
+    public LibroComedia(String titulo, String fechaLanzamiento, String autor, double precio, String editorial,  int stock) {
+        super(titulo, fechaLanzamiento, autor, precio, editorial, Genero.COMEDIA, stock);
+    }
+
+    public LibroComedia() {
+    }
+    
+    
+    public static void registrarLibroComedia(){
+       Scanner leer = new Scanner(System.in);
+       ArrayList<String> datosComun = LibrosUtils.registrarLibroDatosComun();
+       String nombre = datosComun.get(0);
+       String autor = datosComun.get(1);
+       String editorial = datosComun.get(2);
+       LocalDate fecha = LocalDate.parse(datosComun.get(3));
+       double precio = Double.parseDouble(datosComun.get(4));
+       int stock = Integer.parseInt(datosComun.get(5));
+        LibroComedia newLibroComedia = new LibroComedia (nombre, fecha.toString(), autor, precio, editorial, stock);
+        libreria.Libreria.libros.get(Genero.COMEDIA).add(newLibroComedia);
+        System.out.println("El libro ha sido registrado correctamente. Su ID es: "+ newLibroComedia.getID());
+    }
+    public static void eliminarLibroComedia()
+    {
+        Scanner leer = new Scanner(System.in);
+        ArrayList<Libro> librosEliminar = libreria.Libreria.libros.get(Genero.COMEDIA);
+        System.out.println("Ingrese el ID del libro a eliminar:");
+        int id = leer.nextInt();
+            for (Libro libro : librosEliminar) {
+                if (libro.getID()==id) {
+                    libreria.Libreria.libros.get(Genero.COMEDIA).remove(libro);
+                    System.out.println("\n Libro eliminado");
+                    return;
+                }
+            }
+    }
+    
+    public static void mostrarLibroComedia()
+    {
+        LibroComedia libro = new LibroComedia();
+        Scanner leer = new Scanner (System.in);
+        System.out.println("1. Filtrar por precio");                     
+        System.out.println("2. Filtrar por editorial"); 
+        System.out.println("3. Ver todos los libros"); 
+        System.out.print("Ingresa una opcion: ");
+        int opcion = leer.nextInt();
+        switch (opcion) {               
+                case 1:                    
+                    System.out.println("Filtrar por precio");  
+                    System.out.println("Ingresa el precio minimo: ");
+                    double precioInicial = leer.nextDouble();
+                    System.out.println("Ingresa el precio maximo: ");
+                    double precioFinal = leer.nextDouble();
+                    libro.filtrarPorRangoDePrecio(precioInicial, precioFinal);
+                    break;                
+                              
+                case 2:                    
+                    System.out.println("Filtrar por editorial ");
+                    System.out.println("Ingresa el nombre de la editorial: ");
+                    String editorialBuscar = leer.nextLine();
+                    libro.filtrarPorEditorial(editorialBuscar);
+                    break;
+                case 3:                    
+                    System.out.println("**Lista de libros**");  
+                    mostrarLibrosComedia();
+                    break;
+            } 
+        
+    }
+    
+   
+    
+    public static void mostrarLibrosComedia()
+    {
+            for (Libro libro : libreria.Libreria.libros.get(Genero.COMEDIA)) {
+                System.out.println(libro.toString());
+            }
+    }
+    @Override
+    public void filtrarPorEditorial(String editoralBuscada){
+        libreria.Libreria.libros.get(Genero.COMEDIA).stream()
+                .filter(libro -> libro.getEditorial().equals(editoralBuscada))
+                .forEach(libro -> System.out.println(libro.toString()));
+    }
+    @Override
+    public void filtrarPorPrecioMayorA  (double precio){
+        libreria.Libreria.libros.get(Genero.COMEDIA).stream()
+                .filter(libro -> libro.getPrecio() >precio)
+                        .forEach(libro -> System.out.println(libro.toString()));
+    }
+    @Override
+    public void filtrarPorRangoDePrecio(double precioInicial, double precioFinal){
+        libreria.Libreria.libros.get(Genero.COMEDIA).stream()
+                .filter(libro -> libro.getPrecio() <= precioFinal)
+                .filter( libro -> libro.getPrecio() >= precioInicial )
+                .forEach(libro -> System.out.println(libro.toString()));
+    }
+    
+    @Override
+     public void filtrarLibroPorInicialDeAutor(String inicial){
+         libreria.Libreria.libros.get(Genero.COMEDIA)
+                 .stream()
+                 .map(libro->libro.getAutor().toUpperCase())
+                 .forEach(libro-> System.out.println(libro.toString()));
+     }
+}
---
+++ Libreria/Libreria/src/libreria/utils/DatosComun.java
@@ -0,0 +1,112 @@
+
+package libreria.utils;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+import static libreria.Libreria.usuarios;
+import usuarios.Usuario;
+import usuarios.utils.Rol;
+
+public class DatosComun {
+    
+    public   static String [] registrarUsuario(Rol rol){
+        Scanner leer = new Scanner (System.in);
+
+        String rolUsuario = rol == Rol.CLIENTE ? "Cliente" : rol == Rol.ASISTENTE ?"Asistente" : "Gerente";
+        System.out.println(String.format("Bienvenido, comencemos el registro del %s", rolUsuario));
+        
+        String datos[] = new String[6];
+        System.out.print("Ingresa el nombre:: ");
+        String nombre = leer.nextLine();
+        datos[0]=nombre;
+        System.out.print("Ingresa el apellido: ");
+        String apellido = leer.nextLine();
+        datos[1]=apellido;
+        String telefono = registrarTelefonoUsuario(rol);
+        datos[2]=telefono;
+        String nombreUsuario = registrarNombreUsuario(rol);
+        datos[3]=nombreUsuario;
+        System.out.print("Ingresa tu año de nacimiento: ");
+        int YYYY = leer.nextInt();
+        System.out.print("Ingresa tu mes de nacimiento: ");
+        int MM = leer.nextInt();
+        System.out.print("Ingresa tu dia de nacimiento: ");
+        int dd = leer.nextInt();
+        LocalDate fechaNacimiento = LocalDate.of(YYYY, MM, dd);
+        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("dd/MM/YYYY");
+        String fecha = fechaNacimiento.format(pattern);
+        datos[5] = fecha;
+        System.out.print("Ingresa una contrasena: ");
+        String contrasena = leer.nextLine();
+        datos[4]=contrasena;
+        
+        return datos;
+    }
+    
+    
+    
+    private static String registrarTelefonoUsuario(Rol rol){
+        Scanner leer = new Scanner (System.in);
+
+        boolean telefonoExistente = true;
+        String telefono;
+        do{
+        System.out.print("Ingresa el telefono del cliente: ");
+         telefono = leer.nextLine();
+         telefonoExistente = false;
+         if(usuarios!= null && usuarios.containsKey(rol) && !usuarios.get(rol).isEmpty()){
+             ArrayList<Usuario> user = usuarios.get(rol);
+            for(Usuario usuario: user){
+            if(usuario.getTelefono().equals(telefono)){
+                 telefonoExistente=true;
+            }
+            
+            if(telefonoExistente){
+                System.out.println("El telefono ingresado ya esta registrado. Intenta de nuevo. ");
+                break;
+            }
+        }
+         }
+         
+        
+        
+        }while(telefonoExistente);
+        
+        return telefono;
+    }
+    
+    
+    
+    private static String registrarNombreUsuario(Rol rol){
+        Scanner leer = new Scanner (System.in);
+
+         boolean nombreUusarioExistente = true;
+         String nombreUsuario;
+        do{
+           
+            System.out.println("Ingresa un nombre de usuario: ");
+             nombreUsuario = leer.nextLine();
+            nombreUusarioExistente = false;
+            if(usuarios!= null && usuarios.containsKey(rol) && !usuarios.get(rol).isEmpty()){
+            ArrayList<Usuario> user = usuarios.get(rol);
+            for(Usuario usuario: user){
+            if(usuario.getNombreUsuario().equals(nombreUsuario)){
+                 nombreUusarioExistente=true;
+                 break;
+            }
+            if( nombreUusarioExistente)
+                System.out.println("El nombre de usuario ingresado ya esta registrado. Intenta de nuevo. ");
+               
+            
+        }
+            }
+   
+    }while(nombreUusarioExistente);
+       
+        return nombreUsuario;
+  }
+}
